66fa230324388eedcab238d1ea754fc080f0e4b9
le-rich <richardle1997@gmail.com>
2024-04-23 02:32:36 -0700

    ThreeJS with transparency fixed

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ import * as THREE from 'three';
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    alpha: true
+});
+
+var webGLContainer = document.getElementById('WebGLCanvasWrapper');
+webGLContainer.appendChild(renderer.domElement);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
 // Add a cube
 const geometry = new THREE.BoxGeometry();
@@ -16,11 +21,26 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-// Animation loop
-function animate() {
-    requestAnimationFrame(animate);
+const clock = new THREE.Clock()
+
+const tick = () => 
+{
+    const elapsedTime = clock.getElapsedTime();
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+
     renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
 }
-animate();
+tick();
+
+
+// Responsive Listener
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
