be4e1848f54c8b025afd387249222e94df47ffcd
le-rich <richardle1997@gmail.com>
2024-04-23 17:12:09 -0700

    DirkLogo In

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 
 // Set up the scene, camera, and renderer
@@ -13,11 +14,43 @@ webGLContainer.appendChild(renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Add a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new GLTFLoader();
+let dirkLogo = null;
+
+loader.load(
+    'assets/3d/Dirk3DLogoFull.glb', // Path to your GLB file
+    function (gltf) {
+        // Called when the model is loaded
+        const newMaterial = new THREE.MeshStandardMaterial({color: 0x00ff00, metalness: 0.7, roughness: 0.4});
+        gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+                // Adjusting the existing material
+                child.material = newMaterial;
+            }
+        });
+        scene.add(gltf.scene);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded'); // Optional: console logging loading progress
+    },
+    function (error) {
+        console.log('An error happened'); // Optional: console logging loading errors
+    }
+);
+  
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 1, 0); // coming from the top
+scene.add(directionalLight);
+
+// // Add a cube
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 camera.position.z = 5;
 
@@ -26,8 +59,8 @@ const clock = new THREE.Clock()
 const tick = () => 
 {
     const elapsedTime = clock.getElapsedTime();
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
 
     renderer.render(scene, camera);
