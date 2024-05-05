dc5cfbfe1888df4f00b8c8fd0143cf708f16e497
le-rich <richardle1997@gmail.com>
2024-05-05 10:37:47 -0700

    IDK, experimenting

--- js/main.js
+++ js/main.js
@@ -18,21 +18,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const loader = new GLTFLoader();
 let dirkLogo = null;
 
+// let clones = [];
+
 loader.load(
     'assets/3d/Dirk3DLogoFull.glb', // Path to your GLB file
     function (gltf) {
         // Called when the model is loaded
-        const newMaterial = new THREE.MeshStandardMaterial({color: 0x05050a, metalness: 0.0, roughness: 1, opacity: 1, transparent: true, side: THREE.DoubleSide});
+        const newMaterial = new THREE.MeshStandardMaterial({color: 0x05050a, metalness: 0, roughness: 0.4, opacity: 1, transparent: true, side: THREE.DoubleSide});
         gltf.scene.traverse(function (child) {
             if (child.isMesh) {
                 // Adjusting the existing material
                 child.material = newMaterial;
             }
         });
-        gltf.scene.position.set(0,0,-0.1)
+
+        gltf.scene.position.set(0,0,-0.1);
         gltf.scene.scale.set(1.5,1.5,1.5);
         dirkLogo = gltf.scene;
         scene.add(dirkLogo);
+
+        // for (var i = 0; i < 5; ++i){
+        //     let newClone = dirkLogo.clone();
+        //     newClone.position.set(0, 3.4 +i * 3.4, -0.1);
+        //     newClone.scale.set(1.5,1.5,1.5);
+        //     scene.add(newClone)
+        //     clones.push(newClone);
+        // }
     },
     function (xhr) {
         console.log((xhr.loaded / xhr.total * 100) + '% loaded'); // Optional: console logging loading progress
@@ -43,7 +54,7 @@ loader.load(
 );
   
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1); // soft white light
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
@@ -60,15 +71,16 @@ camera.position.z = 15;
 
 const clock = new THREE.Clock()
 
+
+
 const tick = () => 
 {
     const elapsedTime = clock.getElapsedTime();
     if (dirkLogo != null){
-        dirkLogo.rotation.z = (Math.sin(elapsedTime) * 0.5);
+        dirkLogo.position.y = (Math.cos(elapsedTime) * 0.25);
+        dirkLogo.rotation.z = (Math.sin(elapsedTime) * 0.5) * 0.05;
         dirkLogo.rotation.y += 0.005;
     }
-
-
 
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
